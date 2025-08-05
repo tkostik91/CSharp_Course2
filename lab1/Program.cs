0376c13d756adb78ce59666618f0ec9ee7a0caea
Konstantin Tiurin <konstantintiurin@MacBook-Pro-Konstantin.local>
2025-08-05 11:17:33 +0300

    first commi

---
+++ lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab1{
+    class MyThread{
+        int start;
+        int end;
+
+        public MyThread(int start=0, int end=0){
+            this.start =start;
+            this.end = end;
+        }
+
+        public void printRange() {
+            for(int i = start; i <= end; i++){
+                Console.WriteLine($"Thread={Thread.CurrentThread.Name} [i]={i}");
+            }
+        }
+
+    }   
+    internal class Program {
+        
+        static void Main(){
+           Thread t1 = new Thread(new MyThread(1,10).printRange){Name="Thread1"};
+           Thread t2 = new Thread(new MyThread(11,20).printRange){Name="Thread2"};
+
+           t1.Start();
+           t2.Start();
+        }
+    
+    }
+}
