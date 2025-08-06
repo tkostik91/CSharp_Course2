df9bf5538d7ca49efd3003f72ff14e1359798310
Konstantin Tiurin <konstantintiurin@MacBook-Pro-Konstantin.local>
2025-08-06 17:55:37 +0300

    lab1.3

---
+++ SocketClientMT/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+const int CLIENTS = 100;
+ThreadPool.SetMinThreads(CLIENTS, CLIENTS); 
+ThreadPool.SetMaxThreads(CLIENTS, CLIENTS);
+
+Task[] tasks = new Task[CLIENTS];
+for (int i = 0; i < CLIENTS; i++)
+{
+    tasks[i] = new Task(() =>
+    {
+        try
+        {
+            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect("127.0.0.1", 1111);
+            using var stream = new NetworkStream(socket);
+            using var r = new StreamReader(stream, Encoding.UTF8);
+            using var w = new StreamWriter(stream, Encoding.UTF8);
+            w.WriteLine($"hello from {Thread.CurrentThread.ManagedThreadId}");
+            w.Flush();
+            string result = r.ReadLine();
+            Console.WriteLine(result);
+
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    });
+}
+
+foreach (Task t in tasks) t.Start();
+
+Task.WaitAll(tasks);
---
+++ SocketServerMT/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int MAX_CONNECTION_IN_QUEUE = 10; // 10
+const int MAX_THREADS = 20;
+
+//ThreadPool.SetMinThreads(MAX_CONNECTION_IN_QUEUE, MAX_CONNECTION_IN_QUEUE);
+ThreadPool.SetMaxThreads(MAX_THREADS, MAX_THREADS);
+
+IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 1111);
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket.Bind(ipPoint);
+socket.Listen(MAX_CONNECTION_IN_QUEUE);
+int req = 0;
+var s = new object();
+while (true)
+{
+    Socket client = socket.Accept();
+
+   Task.Run(() =>
+    {
+    try
+    {
+        Console.Write($"Remote client: {client.RemoteEndPoint} ");
+            using var stream = new NetworkStream(client);
+            using var r = new StreamReader(stream, Encoding.UTF8);
+            using var w = new StreamWriter(stream, Encoding.UTF8);
+            string result = r.ReadLine();
+            lock (s) req++;
+            Console.WriteLine($"Received: {result}, Requests: {req}");
+            Thread.Sleep(100); //
+
+            w.WriteLine(result.ToUpper());
+            w.Flush();
+            client.Dispose();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    });
+}
+
